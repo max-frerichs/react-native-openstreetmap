358b74ef38353b584d4b37e55869aa7c190ed71c
Max Frerichs <42472887+maxfrerichs@users.noreply.github.com>
2019-06-17 22:28:26 +0200

    Added OSMapPackage constructor

--- android/app/src/main/java/com/osm_implementation/OSMapView.java
+++ android/app/src/main/java/com/osm_implementation/OSMapView.java
@@ -1,28 +1,76 @@
 package com.osm_implementation;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.config.IConfigurationProvider;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-import org.osmdroid.util.MapView
+import static org.osmdroid.tileprovider.util.StorageUtils.getStorage;
 
-public class OSMapView extends MapView {
-    private static final String TAG = MapView.class.getSimpleName();
+public class OSMapView extends Activity {
+    MapView map = null;
 
-    private static final String MAP_TYPE_NORMAL = "normal";
-    private static final String MAP_TYPE_SATELLITE = "satellite";
-    private Map map;
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        map = new MapView(inflater.getContext(), 256, getContext());
-        return map;
+    // Creates map instance from
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Context ctx = getApplicationContext();
+
+        // Checks for required permissions
+        if (ContextCompat.checkSelfPermission(ctx, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Write to external storage permission is not granted
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
+        }
+        if (ContextCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Location access not granted
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        if (ContextCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // Write to external storage permission is not granted
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+        }
+        if (ContextCompat.checkSelfPermission(ctx, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            // Write to external storage permission is not granted
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
+        }
+
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+
+        setContentView(R.layout.map_view);
+        IConfigurationProvider provider = Configuration.getInstance();
+        provider.setUserAgentValue(BuildConfig.APPLICATION_ID);
+        provider.setOsmdroidBasePath(getStorage());
+        provider.setOsmdroidTileCache(getStorage());
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        IMapController mapController = map.getController();
+        map.setMultiTouchControls(true);
+        // sets zoom level and centers the map to
+        mapController.setZoom(9.5);
+        GeoPoint startPoint = new GeoPoint(52.898697, 10.166548);
+        // centers the map on defined coordinates
+        mapController.setCenter(startPoint);
     }
-    public OSMapView(Context context) {
-        super(context);
 
+    public void onResume(){
+        super.onResume();
+        map.onResume();
+    }
 
+    public void onPause(){
+        super.onPause();
+        map.onPause();  
     }
 }
--- android/app/src/main/java/com/osm_implementation/OSMapViewManager.java
+++ android/app/src/main/java/com/osm_implementation/OSMapViewManager.java
@@ -1,22 +1,41 @@
 package com.osm_implementation;
 
+import android.app.Activity;
+
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
-public class OSMapViewManager extends ViewGroupManager<OSMapView> {
-    public static final String REACT_CLASS = "OSMapView";
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
 
+public class OSMapViewManager extends ViewGroupManager<MapView> {
+    public ThemedReactContext mContext = null;
+    private Activity mActivity = null;
+    public static MapController mapController = null;
+    // Defines the
+    public static final String REACT_CLASS = "MapView";
     private static final String TAG = OSMapViewManager.class.getSimpleName();
 
     @Override
     public String getName() {
         return REACT_CLASS;
     }
-
+    public OSMapViewManager(Activity activity) {
+        mActivity = activity;
+    }
     @Override
-    protected OSMapView createViewInstance(ThemedReactContext reactContext) {
-        return new OSMapView (reactContext);
+    protected MapView createViewInstance(ThemedReactContext reactContext) {
+        return new MapView (reactContext);
     }
 
+    /*
+        TODO: Add more properties in further development
+        TODO: Create some spicy tests
+    */
 
+    @ReactProp(name = "zoomLevel")
+    public static void setZoom(MapView view, double zoomLevel) {
+        view.setMaxZoomLevel(zoomLevel);
+    }
 }
